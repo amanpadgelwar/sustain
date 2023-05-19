764c0ac0c20a349b6d11a9aa183b40ab175ae6ef
amanpadgelwar <113186236+amanpadgelwar@users.noreply.github.com>
2023-05-19 23:13:03 +0200

    intial commit

---
+++ src/Pages/Category.js
@@ -0,0 +1,15 @@
+import React from "react";
+import phonesDB from "./phonesDB";
+
+
+export default function Category() {
+  return (
+    <>
+      <h1> iPhones </h1>
+      <ul>
+       
+       Category will be here
+      </ul>
+    </>
+  );
+}
