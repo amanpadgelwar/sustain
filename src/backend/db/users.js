343b5b2ce26ea4967f623ccdbf825a3ff3f18ccf
amanpadgelwar <113186236+amanpadgelwar@users.noreply.github.com>
2023-05-20 20:28:47 +0200

    changed .env path

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -9,10 +9,10 @@ import { formatDate } from "../utils/authUtils";
 export const users = [
   {
     _id: uuid(),
-    firstName: "Adarsh",
-    lastName: "Balika",
-    email: "adarshbalika@gmail.com",
-    password: "adarshbalika",
+    firstName: "Aman",
+    lastName: "Padgelwar",
+    email: "aman123@gmail.com",
+    password: "amanpadgelwar",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
