408a5ffa14041b15622dd83b5af6375134da7ade
amanpadgelwar <113186236+amanpadgelwar@users.noreply.github.com>
2023-05-27 16:55:10 +0200

    modified:   src/App.js #	modified:   src/Pages/Cart.js #	deleted:    src/Pages/CategoryProducts.js #	modified:   src/Pages/ProductDetails.js #	modified:   src/Pages/Wishlist.js #	modified:   src/backend/db/products.js #	deleted:    src/components/CategoryList.js #	deleted:    src/components/ProductCard.js

--- src/Pages/Category.js
+++ src/Pages/Category.js
@@ -1,48 +1,95 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { products } from "../backend/db/products";
-import { CategoryList } from "../components/CategoryList";
+import React, { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
 
 export default function Category() {
   const [categoriesData, setCategoriesData] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+  const { categoryId } = useParams();
 
-  const fetchDataFromServer = async () => {
+  const fetchCategoryData = async () => {
     try {
-      const response = await fetch("/api/categories");
+      const response = await fetch('/api/categories');
       const data = await response.json();
       setCategoriesData(data.categories);
-    } catch (e) {
-      console.error(e);
+    } catch (error) {
+      console.error(error);
     }
-  }
+  };
+
+  const fetchProductsByCategory = async (categoryId) => {
+    try {
+      const response = await fetch(`/api/products?categoryId=${categoryId}`);
+      const data = await response.json();
+      const lowerCaseCategoryId = categoryId.toLowerCase();
+      const categoryProducts = data.products.filter(
+        (product) => product.categoryName.toLowerCase() === lowerCaseCategoryId
+      );
+      setFilteredProducts(categoryProducts);
+    } catch (error) {
+      console.error(error);
+      setFilteredProducts([]);
+    }
+  };
 
   useEffect(() => {
-    fetchDataFromServer();
+    fetchCategoryData();
   }, []);
 
-  const getProductByCategory = (categoryId) => {
-    return products.filter((product) => product.categoryId === categoryId);
-  }
-  
- 
+  useEffect(() => {
+    if (categoryId) {
+      fetchProductsByCategory(categoryId);
+    } else {
+      setFilteredProducts([]);
+    }
+  }, [categoryId]);
 
+  const getCategoryById = (id) => {
+    return categoriesData.find((category) => category._id === id);
+  };
 
   return (
     <>
       <h1>Categories</h1>
-      {categoriesData.map((category) => (
-        <div key={category._id}>
-          <h1>{category.categoryName}</h1>
-          <h3>{category.description}</h3>
-          <h3>{category.imageSrc}</h3>
-          
-          <CategoryList filteredCategory={getProductByCategory(category.id)} noDetail />
-          
-         
-        </div>
-      ))}
-      <Link to="/ProductDetails">See All Products</Link>
+      {categoryId ? (
+        <>
+          <Link to="/category">Return to All Categories</Link>
+          {categoriesData.length > 0 && (
+            <>
+              <h1>{getCategoryById(categoryId)?.categoryName}</h1>
+              <h3>{getCategoryById(categoryId)?.description}</h3>
+              {filteredProducts ? (
+                filteredProducts.length > 0 ? (
+                  <ul>
+                    {filteredProducts.map((product) => (
+                      <div key={product.id}>
+                        <h2>{product.name}</h2>
+                        <p>CategoryName: {product.categoryName}</p>
+                        <p>Description: {product.description}</p>
+                        <p>Price: {product.price}</p>
+                        <p>Image: {product.imageSrc}</p>
+                      </div>
+                    ))}
+                  </ul>
+                ) : (
+                  <p>No products found for the selected category.</p>
+                )
+              ) : (
+                <p>Loading...</p>
+              )}
+            </>
+          )}
+        </>
+      ) : (
+        categoriesData.map((category) => (
+          <div key={category._id}>
+            <h1>{category.categoryName}</h1>
+            <h3>{category.description}</h3>
+            <img src={category.imageSrc} alt="Category" />
+            <Link to={`/products/${category._id}`}>Explore Category</Link>
+            <ProductListingPage cart={cart} onAddToCart={handleAddToCart} />
+          </div>
+        ))
+      )}
     </>
   );
-  
-      }
+}
